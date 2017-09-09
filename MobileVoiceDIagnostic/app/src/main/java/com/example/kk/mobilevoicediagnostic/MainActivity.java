472aba709ba1c4ffbc5e0c38f2984aff7aa82416
kylefirst <kylefirst@gmail.com>
2017-09-09 12:46:27 -0400

    updated drawer panel icons

--- MobileVoiceDIagnostic/app/src/main/java/com/example/kk/mobilevoicediagnostic/MainActivity.java
+++ MobileVoiceDIagnostic/app/src/main/java/com/example/kk/mobilevoicediagnostic/MainActivity.java
@@ -283,17 +283,9 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        if (id == R.id.nav_voice) {
 
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_settings) {
 
         }
 
