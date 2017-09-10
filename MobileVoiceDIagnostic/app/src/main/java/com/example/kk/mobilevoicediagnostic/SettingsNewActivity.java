9da1740b94d4476ade909d509f45f8a9649c4189
kylefirst <kylefirst@gmail.com>
2017-09-10 00:10:17 -0400

    removed fab and old activity

---
+++ MobileVoiceDIagnostic/app/src/main/java/com/example/kk/mobilevoicediagnostic/SettingsNewActivity.java
@@ -0,0 +1,22 @@
+package com.example.kk.mobilevoicediagnostic;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class SettingsNewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings_new);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+}
