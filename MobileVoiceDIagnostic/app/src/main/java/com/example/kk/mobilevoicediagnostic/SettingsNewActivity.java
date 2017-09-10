a959df63975ee055ca4626da5bb4e2e538bccca0
Brian <naiyume@gmail.com>
2017-09-10 04:33:40 -0400

    implemented spinners

--- MobileVoiceDIagnostic/app/src/main/java/com/example/kk/mobilevoicediagnostic/SettingsNewActivity.java
+++ MobileVoiceDIagnostic/app/src/main/java/com/example/kk/mobilevoicediagnostic/SettingsNewActivity.java
@@ -7,8 +7,10 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import static android.provider.AlarmClock.EXTRA_MESSAGE;
@@ -28,6 +30,24 @@ public class SettingsNewActivity extends AppCompatActivity {
         setContentView(R.layout.activity_settings_new);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        Spinner spinner = (Spinner) findViewById(R.id.quantSpinner);
+// Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.quantArray, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+
+        Spinner timeSpinner = (Spinner) findViewById(R.id.timeSpinner);
+// Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> timeAdapter = ArrayAdapter.createFromResource(this,
+                R.array.timeArray, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+        timeAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        timeSpinner.setAdapter(timeAdapter);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
